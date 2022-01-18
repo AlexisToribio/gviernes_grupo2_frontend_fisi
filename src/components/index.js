236766b2dee26440ad83011ea6dcc51792f3b040
FernandoAltamirano <fernando.altamirano@unmsm.edu.pe>
2022-01-18 12:22:59 -0600

    .

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Button } from "./Button";
 export { default as HomeLayout } from "./HomeLayout";
 export { default as HomeInput } from "./HomeInput";
 export { default as InputPicker } from "./InputPicker";
+export { default as Card } from "./Card";
--- src/screens/MyRequest/index.js
+++ src/screens/MyRequest/index.js
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
+import { Card } from '../../components';
+import styles from './styles';
+
+const cardList = [
+  {
+    image:
+      'https://ipmark.com/wp-content/uploads/eventos-de-marketing-2021.jpg',
+    title: 'Conferencia de Big Data',
+    date: 'jueves 19 ago. - 8:00pm',
+    price: 'Gratis',
+    inscription: true,
+    key: 1,
+  },
+  {
+    image:
+      'https://www.protocoloimep.com/app/uploads/2016/11/evento-sostenible.png',
+    title: 'Conferencia de Big Data',
+    date: 'jueves 19 ago. - 8:00pm',
+    price: 'Gratis',
+    inscription: true,
+    key: 2,
+  },
+  {
+    image: 'https://www.esneca.com/wp-content/uploads/eventos-sociales.jpg',
+    title: 'Conferencia de Big Data',
+    date: 'jueves 19 ago. - 8:00pm',
+    price: 'Gratis',
+    inscription: true,
+    key: 3,
+  },
+  {
+    image:
+      'https://www.toulouselautrec.edu.pe/sites/default/files/imagenes/cursos/toulouse-cursos-org-eventos-desktop-37.jpg',
+    title: 'Conferencia de Big Data',
+    date: 'jueves 19 ago. - 8:00pm',
+    price: 'Gratis',
+    inscription: true,
+    key: 4,
+  },
+];
 
 const index = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {cardList.map((card) => {
+          return (
+            <Card
+              image={card.image}
+              title={card.title}
+              date={card.date}
+              price={card.price}
+              inscription={card.inscription}
+              key={card.key}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
